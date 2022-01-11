d5d6f3ec0c35d70c44c9ee31a29783521c1d7ade
PortSwigger Support <support@portswigger.net>
2022-01-11 16:57:36 +0000

    BApp Store preparation

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,17 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.5.30"
     application
+    java
 }
 
 group = "me.user"
 version = "1.2"
+
+// targetCompatibility = 1.8
+// sourceCompatibility = 1.8
+
+java.sourceCompatibility = JavaVersion.VERSION_1_8
+java.targetCompatibility = JavaVersion.VERSION_1_8
 
 repositories {
     mavenCentral()
